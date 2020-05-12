e2e70eb429f1ee9c1ac3bdc6e53c879086fb4928
Steve Turley <steve@turleylabs.com>
2020-05-11 21:19:29 -0500

    Code Smell: Switch Statement. Refactor: Push Down Methods.

--- src/main/java/videostore/ChildrensPriceCode.java
+++ src/main/java/videostore/ChildrensPriceCode.java
@@ -4,4 +4,41 @@ public class ChildrensPriceCode extends PriceCode {
     public ChildrensPriceCode(int priceCode) {
         super(priceCode);
     }
+
+    @Override
+    double calculatePrice(int daysRented) {
+        double thisAmount = 0;
+        switch (getPriceCode()) {
+            case Movie.REGULAR:
+                thisAmount += 2;
+                if (daysRented > 2)
+                    thisAmount += (daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                thisAmount += daysRented * 3;
+                break;
+            case Movie.CHILDRENS:
+                thisAmount += 1.5;
+                if (daysRented > 3)
+                    thisAmount += (daysRented - 3) * 1.5;
+                break;
+        }
+        return thisAmount;
+    }
+
+    @Override
+    int calculateFrequentRenterPoints(int daysRented) {
+        int frequentRenterPoints = 1;
+        switch (getPriceCode()) {
+            case Movie.REGULAR:
+                break;
+            case Movie.NEW_RELEASE:
+                if (daysRented > 1) {
+                    frequentRenterPoints = 2;
+                }
+            case Movie.CHILDRENS:
+                break;
+        }
+        return frequentRenterPoints;
+    }
 }
--- src/main/java/videostore/PriceCode.java
+++ src/main/java/videostore/PriceCode.java
@@ -1,6 +1,6 @@
 package videostore;
 
-public class PriceCode {
+public abstract class PriceCode {
     private int priceCode;
 
     public PriceCode(int priceCode) {
@@ -11,38 +11,7 @@ public class PriceCode {
         return priceCode;
     }
 
-    double calculatePrice(int daysRented) {
-        double thisAmount = 0;
-        switch (getPriceCode()) {
-            case Movie.REGULAR:
-                thisAmount += 2;
-                if (daysRented > 2)
-                    thisAmount += (daysRented - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                thisAmount += daysRented * 3;
-                break;
-            case Movie.CHILDRENS:
-                thisAmount += 1.5;
-                if (daysRented > 3)
-                    thisAmount += (daysRented - 3) * 1.5;
-                break;
-        }
-        return thisAmount;
-    }
+    abstract double calculatePrice(int daysRented);
 
-    int calculateFrequentRenterPoints(int daysRented) {
-        int frequentRenterPoints = 1;
-        switch (getPriceCode()) {
-            case Movie.REGULAR:
-                break;
-            case Movie.NEW_RELEASE:
-                if (daysRented > 1) {
-                    frequentRenterPoints = 2;
-                }
-            case Movie.CHILDRENS:
-                break;
-        }
-        return frequentRenterPoints;
-    }
+    abstract int calculateFrequentRenterPoints(int daysRented);
 }
